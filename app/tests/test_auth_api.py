848f1f9ed334ca44d60f784ab7c98c7fcda359dc
Alve Reduan <alve.reduan.09@gmail.com>
2023-07-13 23:24:16 +0600

    patch

--- app/tests/test_auth_api.py
+++ app/tests/test_auth_api.py
@@ -23,7 +23,7 @@ def generate_user() :
 def test_create_user():
     # Test the creation of a new user
     user_data = generate_user()
-    response = client.post(ENDPOINT + "/users/create", json = user_data)
+    response = client.post(ENDPOINT + "/create", json = user_data)
     assert response.status_code == 201
     user = response.json()
     assert user["email"] == user_data["email"]
@@ -33,7 +33,7 @@ def test_create_user():
 # def test_update_user():
 #     # Test updating a user's password
 #     user_data = generate_user()
-#     response = client.post(ENDPOINT + "/users/create", json = user_data)
+#     response = client.post(ENDPOINT + "/create", json = user_data)
 #     # Authenticate the user and obtain an access token
 #     token = client.post(ENDPOINT + "/login", data = { "username" : user_data["email"], "password" : user_data["password"] })
 #     token = token.json()
@@ -42,7 +42,7 @@ def test_create_user():
 #     assert token["token_type"] == "Bearer"
 #     # Update the user's password with a new value
 #     new_password = "newpassword456"
-#     response = client.put("/users/update", data = { "password" : new_password }, headers = { "Authorization" : f"Bearer {token['access_token']}" })
+#     response = client.put("/update", data = { "password" : new_password }, headers = { "Authorization" : f"Bearer {token['access_token']}" })
 #     assert response.status_code == 201
 #     user = response.json()
 #     assert user["email"] == user_data["email"]
@@ -50,12 +50,12 @@ def test_create_user():
 def test_get_user_profile():
     # Test retrieving a user's profile
     user_data = generate_user()
-    response = client.post(ENDPOINT + "/users/create", json = user_data)
+    response = client.post(ENDPOINT + "/create", json = user_data)
     # Authenticate the user and obtain an access token
     token = client.post(ENDPOINT + "/login", data = { "username" : user_data["email"], "password" : user_data["password"] })
     token = token.json()
     # Retrieve the user's profile using the access token
-    response = client.get(ENDPOINT + "/users/profile", headers = { "Authorization" : f"Bearer {token['access_token']}" })
+    response = client.get(ENDPOINT + "/profile", headers = { "Authorization" : f"Bearer {token['access_token']}" })
     assert response.status_code == 200
     user = response.json()
     assert user["email"] == user_data["email"]
@@ -64,26 +64,26 @@ def test_get_user_profile():
 def test_user_forgot_password():
     # Test the "forgot password" functionality
     user_data = generate_user()
-    response = client.post(ENDPOINT + "/users/create", json = user_data)
+    response = client.post(ENDPOINT + "/create", json = user_data)
     user = response.json()
-    response = client.post(ENDPOINT + "/users/forgot", data = { "email" : user["email"] })
+    response = client.post(ENDPOINT + "/forgot", data = { "email" : user["email"] })
     assert response.status_code == 200
     assert response.json()['message'] == 'Verification Code Sent successfully'
     
 
 def test_user_validate():
     # Test validating a user's code
-    response = client.post(ENDPOINT + "/users/validate", data = { "code_from_user" : 123 })
+    response = client.post(ENDPOINT + "/validate", data = { "code_from_user" : 123 })
     assert response.status_code == 404
     assert response.json()['detail']['message'] == 'Invalid Code Provided'
 
 
 # def test_cities() :
-#     response = client.get(ENDPOINT + "/users/city")
+#     response = client.get(ENDPOINT + "/city")
 #     assert response.status_code == 200
     
 # def test_center() :
-#     response = client.get(ENDPOINT + "/users/center")
+#     response = client.get(ENDPOINT + "/center")
 #     assert response.status_code == 200
     
 
